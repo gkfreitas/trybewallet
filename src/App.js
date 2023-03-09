ab5ce1953d8e845edb7490945c1e8fb86f4301d8
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-03-09 15:12:54 -0300

    Iniciando projeto - Trybe wallet, projeto configurado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { legacy_createStore as createStore } from 'redux';
+import rootReducer from './redux/reducers/index';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    );
+  }
 }
-
-export default App;
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+const INITIAL_STATE = {
+  email: '', // string que armazena o email da pessoa usuária
+
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
