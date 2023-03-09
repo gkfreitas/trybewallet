8f93f3986e24dbd3fcff08456d3b0fa996b96d9b
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-03-09 17:37:32 -0300

    REQ2- Finalizado, componente header criado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,29 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { emailState } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <h1 data-testid="email-field">{emailState}</h1>
+        <p data-testid="total-field">
+          0
+          {' '}
+          <span data-testid="header-currency-field">BRL</span>
+        </p>
+      </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  emailState: state.user.email,
+});
+
+Header.propTypes = {
+  emailState: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
